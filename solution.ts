f163bc4b1cdfe14557b7dd27cb19fe148484e718
Piash Islam <piashislam2k@gmail.com>
2026-04-19 19:30:17 +0600

    added solution for problem 2

--- solution.ts
+++ solution.ts
@@ -2,3 +2,21 @@ const reverseStr = (str: string): string => {
     return str.split("").reverse().join("");
 }
 // console.log(reverseStr("I am a good boy"))
+
+const fizzbuzz = (n: number): string[] => {
+    const result: string[] = [];
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            result.push("FizzBuzz");
+        } else if (i % 3 === 0) {
+            result.push("Fizz");
+        } else if (i % 5 === 0) {
+            result.push("Buzz");
+        } else {
+            result.push(i.toString());
+        }
+    }
+    return result;
+}
+
+// console.log(fizzbuzz(35))
