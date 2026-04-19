3dd8889c847fa288ba3a00db95ecbc4402e9e345
Piash Islam <piashislam2k@gmail.com>
2026-04-19 22:27:14 +0600

    added solution for problem 12

--- solution.ts
+++ solution.ts
@@ -111,3 +111,17 @@ const multiplicationTable = (n: number): string[] => {
 };
 
 // console.log(multiplicationTable(5))
+
+
+
+const isPrime  =(n: number):boolean=>{
+    if(n<=1) return false;
+    if(n===2) return true;
+    for(let i=2; i<=Math.sqrt(n); i++){
+        if(n%i===0){
+            return false
+        }
+    }
+    return true
+}
+// console.log(isPrime(7))
